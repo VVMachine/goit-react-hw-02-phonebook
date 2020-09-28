d7c55d8811469ac0b8da04529aefea443c70cc4b
VVMachine <vik.atamanenko@gmail.com>
2020-09-28 14:04:01 +0300

    Updates

---
+++ precache-manifest.e6541b03e07886a78fc98294459f662f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef22f8e2071cd732ddefef8906d3ed5e",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "5a009cc0ca85dcabe4b7",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.35cebf9b.chunk.css"
+  },
+  {
+    "revision": "8079a4782a76aea0204d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.dde26a2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.dde26a2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a009cc0ca85dcabe4b7",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.9cdf1792.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
